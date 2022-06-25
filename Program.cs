e565cb4951096dfb4e866bb5a135d7b99ec36911
dayane <dayane.barrosoliveira@gmail.com>
2022-06-25 11:21:34 -0300

    GeraEstoque v1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace GeraEstoqueNew// Note: actual namespace depends on the project name.
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Menu.Menus();
+
+     Cadastrar.CadastrarProdutos();
+
+    }
+  }
+}
